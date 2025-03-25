dbaecdf6c944be010ee49600edf1fdf626959dc8
yush-nh <yushin.nihei@luxiar.com>
2025-03-25 10:07:46 +0900

    Update note search form test

--- test/models/note_search_form_test.rb
+++ test/models/note_search_form_test.rb
@@ -10,7 +10,7 @@ class NoteSearchFormTest < ActiveSupport::TestCase
 
   test "should searchable by title" do
     form = NoteSearchForm.new(title: "aaa")
-    result = form.search_ids(Note, "dummy-cache-key")
+    result = form.search_ids("dummy-cache-key")
 
     assert_includes result, @note1.id
     assert_not_includes result, @note2.id
@@ -18,7 +18,7 @@ class NoteSearchFormTest < ActiveSupport::TestCase
 
   test "should searchable by author" do
     form = NoteSearchForm.new(author: "aaa@example.com")
-    result = form.search_ids(Note, "dummy-cache-key")
+    result = form.search_ids("dummy-cache-key")
 
     assert_includes result, @note1.id
     assert_not_includes result, @note2.id
@@ -26,7 +26,7 @@ class NoteSearchFormTest < ActiveSupport::TestCase
 
   test "should searchable by updated_at start date" do
     form = NoteSearchForm.new(start_date: 2.days.ago.to_date)
-    result = form.search_ids(Note, "dummy-cache-key")
+    result = form.search_ids("dummy-cache-key")
 
     assert_includes result, @note1.id
     assert_not_includes result, @note2.id
@@ -34,7 +34,7 @@ class NoteSearchFormTest < ActiveSupport::TestCase
 
   test "should searchable by updated_at end date" do
     form = NoteSearchForm.new(end_date: 2.days.ago.to_date)
-    result = form.search_ids(Note, "dummy-cache-key")
+    result = form.search_ids("dummy-cache-key")
 
     assert_includes result, @note2.id
     assert_not_includes result, @note1.id
@@ -43,7 +43,7 @@ class NoteSearchFormTest < ActiveSupport::TestCase
   test "should searchable by updated_at start and end date" do
     note3 = Note.create!(title: "ccc", updated_at: 5.day.ago, user: @user1)
     form = NoteSearchForm.new(start_date: 4.days.ago.to_date, end_date: 2.days.ago.to_date)
-    result = form.search_ids(Note, "dummy-cache-key")
+    result = form.search_ids("dummy-cache-key")
 
     assert_includes result, @note2.id
     assert_not_includes result, @note1.id
